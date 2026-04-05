376bccfb771093bab569ac4600cfdaa094e50dd8
Miconen <mico.rintala@gmail.com>
2026-04-05 18:48:46 +0300

    Formatted file

--- src/main/java/weighttables/WeightFunctions.java
+++ src/main/java/weighttables/WeightFunctions.java
@@ -1,20 +1,18 @@
 package weighttables;
+
 import java.util.Random;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class WeightFunctions {
-    
-    public static int getTotalWeight(JSONArray UniquesList)
-    {
+
+    public static int getTotalWeight(JSONArray UniquesList) {
         int totalWeight = 0;
 
-        for (Object entry : UniquesList)
-        {
+        for (Object entry : UniquesList) {
             JSONObject item = (JSONObject) entry;
-            if (item.containsKey("weight"))
-            {
+            if (item.containsKey("weight")) {
                 int weight = ((Long) item.get("weight")).intValue();
                 totalWeight += weight;
             }
@@ -23,36 +21,38 @@ public class WeightFunctions {
         return totalWeight;
     }
 
-    public static JSONObject rollItem(JSONArray ItemList)
-    {
+    public static JSONObject rollItem(JSONArray ItemList) {
         int totalWeight = getTotalWeight(ItemList);
 
-        if (totalWeight <= 0) {return null;}
+        if (totalWeight <= 0) {
+            return null;
+        }
 
         Random r = new Random();
-        int roll = r.nextInt(totalWeight+1);
+        int roll = r.nextInt(totalWeight + 1);
 
         JSONObject output = null;
 
         int weight = -1;
 
-        for (Object entry : ItemList)
-        {
+        for (Object entry : ItemList) {
             JSONObject item = (JSONObject) entry;
 
-            if (item.containsKey("weight"))
-            {
+            if (item.containsKey("weight")) {
                 weight = ((Long) item.get("weight")).intValue();
-                if (roll <= weight) 
-                { return output = item;}
-                else 
-                { roll -= weight;}
+                if (roll <= weight) {
+                    return output = item;
+                } else {
+                    roll -= weight;
+                }
             }
 
             // If the item does not have a weight, skip over it
         }
 
-        if (weight <= -1) {return null;}
+        if (weight <= -1) {
+            return null;
+        }
 
         return output;
     }
